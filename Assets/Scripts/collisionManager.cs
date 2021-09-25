b8307f38a6579802a72532f86e5aa1b782ab3915
NikThomas9 <nikrthomas@gmail.com>
2021-09-25 15:54:03 -0700

    please dont break

---
+++ Assets/Scripts/collisionManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionManager : MonoBehaviour
+{
+    [SerializeField] private Vector3 growthVector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Collectible")
+        {
+            transform.localScale += growthVector;
+            Destroy(col.gameObject);
+            transform.position += new Vector3(0, growthVector.y, 0);
+        }
+    }
+}
