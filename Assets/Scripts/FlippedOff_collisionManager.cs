cb899702ebfc65fe24389a95a0dd45807d94d982
NikThomas9 <nikrthomas@gmail.com>
2021-09-26 13:00:12 -0700

    remove bad files

---
+++ Assets/Scripts/FlippedOff_collisionManager.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlippedOff_collisionManager : MonoBehaviour
+{
+    [SerializeField] private float currentSize = 0; 
+    [SerializeField] private AudioSource collectSFX;
+    public BoxCollider playerCol;
+    public Transform pivotLeft;
+    public Transform pivotRight;
+    public Transform pivotForward;
+    public Transform pivotBackward;
+    public FlippedOff_cameraShake shakeController;
+    private Vector3 growthValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCol = gameObject.GetComponent<BoxCollider>();
+        Debug.Log(GameController.Instance.gameDifficulty);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Collectible")
+        {
+            if (currentSize >= col.gameObject.GetComponent<FlippedOff_collectible>().sizeRequired)
+            {
+                collectSFX.Play();
+                //Chomp SFX
+                
+                if (col.gameObject.GetComponent<FlippedOff_collectible>().winningCollectible)
+                    GameController.Instance.WinGame();
+                //Box grows larger
+                growthValue = col.gameObject.GetComponent<FlippedOff_collectible>().growthValue;
+                transform.localScale += growthValue;
+                shakeController.shakeMagnitude += 0.05f;
+
+                Destroy(col.gameObject);
+                currentSize += col.gameObject.GetComponent<FlippedOff_collectible>().pointsGiven;
+                pivotReposition();
+            }
+            else
+            {
+                //Boink SFX
+                reverseDirection();
+
+                Debug.Log("Can't be eaten!");
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Boundary")
+            reverseDirection();
+    }
+
+    void reverseDirection()
+    {
+        print("reversing direction");
+        FlippedOff_playerMovement player = playerCol.gameObject.GetComponent<FlippedOff_playerMovement>();
+
+        //reverse direction of rotation
+        if (player.rotateAxis == Vector3.right || player.rotateAxis == Vector3.left)
+            player.direction.x = -player.direction.x;
+        else
+            player.direction.z = -player.direction.z;
+
+        //reverse axis of rotation
+        player.rotateAxis = -player.rotateAxis;
+
+        //set rotation amount to the remained of the original rotation
+        player.totalRotation = 90f - player.totalRotation;
+    }
+
+    void pivotReposition()
+    {
+        pivotLeft.localPosition = new Vector3(-playerCol.transform.localScale.x / 2, -playerCol.transform.localScale.x / 2, 0);
+
+        pivotBackward.localPosition = new Vector3(0, -playerCol.transform.localScale.x / 2, -playerCol.transform.localScale.x / 2);
+
+        pivotForward.localPosition = new Vector3(0, -playerCol.transform.localScale.x / 2, playerCol.transform.localScale.x / 2);
+
+        pivotRight.localPosition = new Vector3(playerCol.transform.localScale.x / 2, -playerCol.transform.localScale.x / 2, 0);
+    }
+}
---
+++ Assets/Scripts/FlippedOff_playerMovement.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlippedOff_playerMovement : MonoBehaviour
+{
+    [SerializeField] private GameObject pivotPoints;
+    [SerializeField] private AudioSource slamSFX;
+    private bool moving = false;
+    public FlippedOff_cameraShake shakeController;
+    [HideInInspector] public Vector3 direction;
+    [HideInInspector] public float totalRotation = 0f;
+    [HideInInspector] public Vector3 rotateAxis;
+
+    public int rotateSpeed = 180;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetMoveDirection();
+        if (moving) 
+        {
+            totalRotation += rotateSpeed * Time.deltaTime;
+            if(totalRotation > 90f)
+            {
+                transform.RotateAround(direction, rotateAxis, rotateSpeed * Time.deltaTime - (totalRotation - 90f));
+            }    
+            else
+            {
+                transform.RotateAround(direction, rotateAxis, rotateSpeed * Time.deltaTime);
+            } 
+        }
+        if(totalRotation >= 90f)
+        {
+            moving = false;
+            totalRotation = 0f;
+            transform.position = new Vector3(transform.position.x, transform.lossyScale.y / 2, transform.position.z);
+            shakeController.TriggerShake();
+            slamSFX.Play();
+        }
+    }
+
+    private void GetMoveDirection()
+    {
+        if (!moving)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                moving = true;
+                direction = pivotPoints.transform.GetChild(0).position;
+                rotateAxis = Vector3.right;
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                moving = true;
+                direction = pivotPoints.transform.GetChild(1).position;
+                rotateAxis = Vector3.forward;
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                moving = true;
+                direction = pivotPoints.transform.GetChild(2).position;
+                rotateAxis = Vector3.back;
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                moving = true;
+                direction = pivotPoints.transform.GetChild(3).position;
+                rotateAxis = Vector3.left;
+            }
+
+        }
+        
+    }
+}
